1efd4b47c5f8e3d8a735b81594282319823b3338
AndyCatlow <mrcatlow@gmail.com>
2022-07-19 22:11:20 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import GlobalStyle from "./components/GlobalStyle";
 // Import Pages
 import Nav from "./components/Nav";
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Routes>
+        <Route path="/" exact element={<AboutUs />} />
+        <Route path="/about" exact element={<AboutUs />} />
+        <Route path="/work" exact element={<OurWork />} />
+        <Route path="/contact" exact element={<ContactUs />} />
+      </Routes>
     </div>
   );
 }
